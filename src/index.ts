0f1c8b862abf7790cfb634c4dead7c8b2a727a7e
arman <arman@noca.pro>
2025-09-20 14:02:33 +0700

    ready to push

--- src/index.ts
+++ src/index.ts
@@ -49,4 +49,4 @@ export {
 } from './utils/notifier';
 export { STATE_DIRECTORY_NAME, CONFIG_FILE_NAMES } from './utils/constants';
 
-export const __LAST_MODIFIED__ = '2025-09-20 05:37:04';
+export const __LAST_MODIFIED__ = '2025-09-20 07:02:09';
